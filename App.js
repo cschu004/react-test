7c57cd74297eb835fa7d1c64ae180d64b3bd0883
cschu004 <cschu004@gmail.com>
2021-06-05 17:33:23 -0700

    halp me an

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const loadpic = {require('./IcePick_64x64.gif')}
+
 const App = () => {
   return (
     <View style={styles.container}>
+      <Image source loadpic />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
